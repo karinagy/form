b2fe340797fa392aace6eb0dd862482ba17ffb47
Karina Rabcovskaya <karina6918538@gmail.com>
2023-01-09 17:36:24 +0300

    first commit

---
+++ form_pr/task_form/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+from .models import ModelForm
+
+
+@admin.register(ModelForm)
+class ModelFormAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'phone', 'date', 'text')
+    empty_value_display = '-пусто-'
